d4c3170a8be9bba4318a7ddc2cfcef66090ed042
Wadad Parker <wadadparker5@gmail.com>
2024-09-11 06:37:33 +0530

    feat: background color

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <div className=" bg-custom-radial h-screen w-screen">
+      <h1 className="text-blue-300">Hello World</h1>  
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+
+      },
+      backgroundImage: {
+        'custom-radial': 'radial-gradient(circle at bottom, #41004E , #1C0022 25%, #120015 40%, #130016 40%, #000000 70%)',
+      },
+    },
+  },
+  plugins: [],
+}
